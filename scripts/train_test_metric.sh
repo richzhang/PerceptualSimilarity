5155a33d28b27500c5f70d45fa9ffac3306e0779
Richard Zhang <rzhang88@gmail.com>
2019-07-06 17:03:30 -0700

    mkdir

--- scripts/train_test_metric.sh
+++ scripts/train_test_metric.sh
@@ -1,5 +1,6 @@
 
 TRIAL=${1}
 NET=${2}
+mkdir checkpoints/${NET}_${TRIAL}
 python ./train.py --use_gpu --net ${NET} --name ${NET}_${TRIAL}
 python ./test_dataset_model.py --use_gpu --net ${NET} --model_path ./checkpoints/${NET}_${TRIAL}/latest_net_.pth
--- scripts/train_test_metric_tune.sh
+++ scripts/train_test_metric_tune.sh
@@ -1,5 +1,6 @@
 
 TRIAL=${1}
 NET=${2}
+mkdir checkpoints/${NET}_${TRIAL}_tune
 python ./train.py --train_trunk --use_gpu --net ${NET} --name ${NET}_${TRIAL}_tune
 python ./test_dataset_model.py --train_trunk --use_gpu --net ${NET} --model_path ./checkpoints/${NET}_${TRIAL}_tune/latest_net_.pth
