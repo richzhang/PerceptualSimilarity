4fa3cb6f43f90fc042ddb0b73eeb9da843bd6a3e
ishengfang <nf0126@gmail.com>
2019-07-10 17:21:07 +0800

    clean

--- data/data_loader.py
+++
@@ -1,6 +0,0 @@
-def CreateDataLoader(datafolder,dataroot='./dataset',dataset_mode='2afc',load_size=64,batch_size=1,serial_batches=True):
-    from data.custom_dataset_data_loader import CustomDatasetDataLoader
-    data_loader = CustomDatasetDataLoader()
-    # print(data_loader.name())
-    data_loader.initialize(datafolder,dataroot=dataroot+'/'+dataset_mode,dataset_mode=dataset_mode,load_size=load_size,batch_size=batch_size,serial_batches=serial_batches, nThreads=1)
-    return data_loader
