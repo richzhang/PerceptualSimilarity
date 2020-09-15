9cff7b42c448756119fd9e479db367aae6e30212
Richard Zhang <rich.zhang@eecs.berkeley.edu>
2020-09-15 17:46:49 +0000

    change setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
      name='lpips',  
-     version='0.1',
+     version='0.1.1',
      author="Richard Zhang",
      author_email="rizhang@adobe.com",
      description="LPIPS Similarity metric",
