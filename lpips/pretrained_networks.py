bfacc6a621ed772dd29c030a459820e4a94843e7
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2021-08-26 11:41:23 -0700

    model load hack

--- lpips/pretrained_networks.py
+++ lpips/pretrained_networks.py
@@ -97,6 +97,7 @@ class vgg16(torch.nn.Module):
     def __init__(self, requires_grad=False, pretrained=True):
         super(vgg16, self).__init__()
         vgg_pretrained_features = tv.vgg16(pretrained=pretrained).features
+        vgg_pretrained_features.load_state_dict(torch.load('carl_vgg16.pt'))
         self.slice1 = torch.nn.Sequential()
         self.slice2 = torch.nn.Sequential()
         self.slice3 = torch.nn.Sequential()
