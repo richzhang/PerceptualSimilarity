907f23523545256d20352f1d17c00000f620a32d
Richard Zhang <rzhang88@gmail.com>
2023-10-24 16:06:20 -0700

    Update download_dataset.sh

--- scripts/download_dataset.sh
+++ scripts/download_dataset.sh
@@ -2,18 +2,18 @@
 mkdir dataset
 
 # JND Dataset
-wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz
+wget https://perceptual-similarity.s3.us-west-2.amazonaws.com/dataset/jnd.tar.gz -O ./dataset/jnd.tar.gz
 
 mkdir dataset/jnd
-tar -xzf ./dataset/jnd.tar.gz -C ./dataset
+tar -xf ./dataset/jnd.tar.gz -C ./dataset
 rm ./dataset/jnd.tar.gz
 
 # 2AFC Val set
 mkdir dataset/2afc/
-wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz
+wget https://perceptual-similarity.s3.us-west-2.amazonaws.com/dataset/twoafc_val.tar.gz -O ./dataset/twoafc_val.tar.gz
 
 mkdir dataset/2afc/val
-tar -xzf ./dataset/twoafc_val.tar.gz -C ./dataset/2afc
+tar -xf ./dataset/twoafc_val.tar.gz -C ./dataset/2afc
 rm ./dataset/twoafc_val.tar.gz
 
 # 2AFC Train set
@@ -21,5 +21,5 @@ mkdir dataset/2afc/
 wget https://people.eecs.berkeley.edu/~rich.zhang/projects/2018_perceptual/dataset/twoafc_train.tar.gz -O ./dataset/twoafc_train.tar.gz
 
 mkdir dataset/2afc/train
-tar -xzf ./dataset/twoafc_train.tar.gz -C ./dataset/2afc
+tar -xf ./dataset/twoafc_train.tar.gz -C ./dataset/2afc
 rm ./dataset/twoafc_train.tar.gz
